a54901ab5505bbae67886044afa40df0815c3b2b
easter1201 <kite1201kr@gmail.com>
2025-05-27 20:20:00 +0900

    이름 수정기능 추가

--- src/main/java/com/example/newspeed/dto/UsersResponseDto.java
+++ src/main/java/com/example/newspeed/dto/UsersResponseDto.java
@@ -1,4 +1,4 @@
 package com.example.newspeed.dto;
 
-public class UsersResponseDTO {
+public class UsersResponseDto {
 }
--- src/main/java/com/example/newspeed/service/UsersService.java
+++ src/main/java/com/example/newspeed/service/UsersService.java
@@ -1,4 +1,8 @@
 package com.example.newspeed.service;
 
+import com.example.newspeed.dto.*;
+
+
 public interface UsersService {
+    UsersResponseDto updateUsersName(Long userId, UpdateUserNameRequestDto updateRequest);
 }
