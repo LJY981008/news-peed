9d6f20e5f934aca89bd48b36d30565ed81600f2c
이준영 <tera199810@gmail.com>
2025-05-27 16:58:38 +0900

    first commit

---
+++ src/main/java/com/example/newspeed/NewsPeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.newspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPeedApplication.class, args);
+	}
+
+}
