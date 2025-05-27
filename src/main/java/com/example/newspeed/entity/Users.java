d8b373d4b92ac914a501878e491401ecb8bac069
easter1201 <kite1201kr@gmail.com>
2025-05-27 19:35:45 +0900

    add Users.java in entity and add UsersRepository in repository

--- src/main/java/com/example/newspeed/entity/Users.java
+++ src/main/java/com/example/newspeed/entity/Users.java
@@ -3,12 +3,14 @@ package com.example.newspeed.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-public class User extends BaseEntity{
+@NoArgsConstructor
+public class Users extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
@@ -17,4 +19,12 @@ public class User extends BaseEntity{
     private String userName;
     private String intro;
     private String profileImageUrl;
+
+    public Users(String email, String password, String userName, String intro, String profileImageUrl){
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+        this.intro = intro;
+        this.profileImageUrl = profileImageUrl;
+    }
 }
--- src/main/java/com/example/newspeed/repository/UsersRepository.java
+++ src/main/java/com/example/newspeed/repository/UsersRepository.java
@@ -1,2 +1,7 @@
-package com.example.newspeed.repository;public class UsersRepository {
+package com.example.newspeed.repository;
+
+import com.example.newspeed.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
 }
