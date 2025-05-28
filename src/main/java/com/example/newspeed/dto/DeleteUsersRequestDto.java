5dd270c83babeafad7f9c2eecfa16060e5cb6bb6
easter1201 <kite1201kr@gmail.com>
2025-05-28 17:21:42 +0900

    Feat: update / delete dto 제약 추가, controller 쿠키인증 제거 및 이메일 기반 서치 추가, userService 이메일 기반 서치 및 주석 추가

--- src/main/java/com/example/newspeed/dto/DeleteUsersRequestDto.java
+++ src/main/java/com/example/newspeed/dto/DeleteUsersRequestDto.java
@@ -1,12 +1,14 @@
 package com.example.newspeed.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
 public class DeleteUsersRequestDto {
-    @NotBlank
-    @Size
+    @NotBlank(message = "이메일은 필수 입력사항입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+    @NotBlank(message = "비밀번호는 필수 입력사항입니다.")
+    @Size(min = 4, max = 20, message = "비밀번호의 길이는 4~20자입니다.")
     private String password;
 }
