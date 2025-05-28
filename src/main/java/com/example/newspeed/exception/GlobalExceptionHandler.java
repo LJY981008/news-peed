583b3b28669a80be16524bdd3ec3fc5aa43a0c09
이준영 <tera199810@gmail.com>
2025-05-28 11:35:22 +0900

    Feat : Validation 예외 처리

--- src/main/java/com/example/newspeed/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/newspeed/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.example.newspeed.exception;
 import com.example.newspeed.exception.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -25,5 +27,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, Object>> handleNotFoundException(NotFoundException e) {
         Map<String, Object> errors = Map.of("message", e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
+    }
+
+    /**
+     * Validation에 대한 예외 처리
+     *
+     * @param e 발생한 예외 객체
+     * @return 상태메세지와 에러코드 반환
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        Map<String, Object> errors = Map.of("message", bindingResult.getFieldError().getDefaultMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
