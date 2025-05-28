b30441f89f8806cffed7d2e34dcc8582c17f2444
taehyeon <max5915@naver.com>
2025-05-28 12:26:11 +0900

    feat : 조회기능

--- src/main/java/com/example/newspeed/controller/PostController.java
+++ src/main/java/com/example/newspeed/controller/PostController.java
@@ -1,9 +1,16 @@
 package com.example.newspeed.controller;
 
 
+import com.example.newspeed.dto.Post.FindPostResponseDto;
+import com.example.newspeed.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/news-peed")
@@ -11,4 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+
+
+    //게시글 조회
+    @GetMapping
+    public ResponseEntity<List<FindPostResponseDto>> findPost() {
+        List<FindPostResponseDto> findPostResponseDtoList = postService.findPost();
+        return new ResponseEntity<>(findPostResponseDtoList, HttpStatus.OK);
+    }
 }
