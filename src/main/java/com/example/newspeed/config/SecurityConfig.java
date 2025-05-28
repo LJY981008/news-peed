582a52caf481566088dbc48e52d08b663d6f6c2e
이준영 <tera199810@gmail.com>
2025-05-28 12:58:50 +0900

    Feat : 시큐리티 설정 추가

--- src/main/java/com/example/newspeed/config/SecurityConfig.java
+++ src/main/java/com/example/newspeed/config/SecurityConfig.java
@@ -1,4 +1,55 @@
 package com.example.newspeed.config;
 
+import com.example.newspeed.Filter.JwtFilter;
+import com.example.newspeed.exception.authentication.JwtAccessDeniedHandler;
+import com.example.newspeed.exception.authentication.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    //TODO authorizeHttpRequests 롤 설정 필요
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .cors(Customizer.withDefaults()) //Cross-Origin Resource Sharing
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session
+                        -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests( auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/news-peed/comments").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/news-peed/comments").hasRole("User")
+                        .requestMatchers(HttpMethod.DELETE, "/news-peed/comments").hasRole("User")
+                        .requestMatchers(HttpMethod.PATCH, "/news-peed/comments").hasRole("User")
+                        .anyRequest().permitAll()
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/news-peed/logout").authenticated()
+                )
+                .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling( configure -> configure
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                .build();
+    }
 }
