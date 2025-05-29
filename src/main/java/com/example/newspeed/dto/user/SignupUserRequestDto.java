9cdbed2a06deb88f2703bde0db934ff325763c73
LeeHyeonha <lej5769@gmail.com>
2025-05-29 19:22:49 +0900

    Feat: 회원가입 시 필수 필드 미입력 시 메세지 추가

--- src/main/java/com/example/newspeed/dto/user/SignupUserRequestDto.java
+++ src/main/java/com/example/newspeed/dto/user/SignupUserRequestDto.java
@@ -1,13 +1,21 @@
 package com.example.newspeed.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class SignupUserRequestDto {
 
+    @NotBlank(message = "Not entered email")
     private String email;
+
+    @NotBlank(message = "Not entered password")
     private String password;
+
+    @NotBlank(message = "Not entered userName")
     private String userName;
+
     private String intro;
+
     private String profileImageUrl;
 }
