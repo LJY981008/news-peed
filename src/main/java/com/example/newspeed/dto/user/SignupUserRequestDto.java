c11c03497f6460e5dba3585a2d06a8a2efcf8426
LeeHyeonha <lej5769@gmail.com>
2025-05-29 20:10:33 +0900

    Feat: 회원가입 시 이메일 형식 에러 메시지 추가

--- src/main/java/com/example/newspeed/dto/user/SignupUserRequestDto.java
+++ src/main/java/com/example/newspeed/dto/user/SignupUserRequestDto.java
@@ -1,11 +1,13 @@
 package com.example.newspeed.dto.user;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class SignupUserRequestDto {
 
+    @Email(message = "Invalid email format")
     @NotBlank(message = "Not entered email")
     private String email;
 
