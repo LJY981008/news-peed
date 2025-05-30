20adabfc3074a19bf76c3a83a2ced797a11da3fa
LeeHyeonha <lej5769@gmail.com>
2025-05-30 17:11:13 +0900

    Comment: 주석 추가

--- src/main/java/com/example/newspeed/repository/UserRepository.java
+++ src/main/java/com/example/newspeed/repository/UserRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * <p>유저 테이블 레포지토리</p>
+ *
+ * @author 이현하
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     List<User> findByUserName(String usrName);
