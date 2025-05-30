8fd9c0dc8a75a59a362898ed600c610d2bb54601
이준영 <tera199810@gmail.com>
2025-05-30 10:47:44 +0900

    feat : find query 작성

--- src/main/java/com/example/newspeed/repository/LikeRepositoryCustom.java
+++ src/main/java/com/example/newspeed/repository/LikeRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.example.newspeed.repository;
 
+import com.example.newspeed.entity.PostLike;
+
+import java.util.Optional;
+
 public interface LikeRepositoryCustom {
+    Optional<PostLike> findByPostIdAndUserId(Long postId, Long userId);
 }
--- src/main/java/com/example/newspeed/repository/LikeRepositoryCustomImpl.java
+++ src/main/java/com/example/newspeed/repository/LikeRepositoryCustomImpl.java
@@ -1,7 +1,12 @@
 package com.example.newspeed.repository;
 
 import com.example.newspeed.entity.PostLike;
+import com.example.newspeed.entity.QPostLike;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public class LikeRepositoryCustomImpl implements LikeRepositoryCustom {
 
@@ -11,4 +16,14 @@ public class LikeRepositoryCustomImpl implements LikeRepositoryCustom {
         this.queryFactory = queryFactory;
     }
 
+    @Override
+    public Optional<PostLike> findByPostIdAndUserId(Long postId, Long userId) {
+        QPostLike postLike = QPostLike.postLike;
+
+        return Optional.ofNullable(queryFactory
+                .selectFrom(postLike)
+                .where(postLike.post.postId.eq(postId), postLike.user.userId.eq(userId))
+                .fetchOne()
+        );
+    }
 }
